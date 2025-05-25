a0bd1c98288b78a0e77b3f530af1bdecea8f9186
seungjun <seungzun01@gmail.com>
2025-05-25 14:47:03 +0900

    Update Hompage_Carousel

--- client/src/pages/js/Home.js
+++ client/src/pages/js/Home.js
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import '../css/Home.css';
-
+import ClubCarousel
+ from './ClubCarousel';
 function Home() {
   const navigate = useNavigate();
 
   return (
     <div className="home-container">
+          <div className="carousel-section">
+        <h3>참여 동아리</h3>
+        <ClubCarousel />
+      </div>
       <h1 className="project-title">SWingSpace</h1>
       <div className="button-group">
         <button onClick={() => navigate('/signup')} className="home-button">회원가입</button>
