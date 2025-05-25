17c394875c7bacce3227584c95ccacb17c628ffd
seungjun <seungzun01@gmail.com>
2025-05-25 18:01:41 +0900

    기존~

--- client/src/pages/js/ClubCarousel.js
+++ client/src/pages/js/ClubCarousel.js
@@ -2,8 +2,8 @@ import '../css/ClubCarousel.css';
 
 import club1 from '../../assets/logos/AIM.png';
 import club2 from '../../assets/logos/FOSCAR.png';
-import club3 from '../../assets/logos/DOUM.png';
-import club4 from '../../assets/logos/KOBOT.png';
+import club3 from '../../assets/logos/KOBOT.png';
+import club4 from '../../assets/logos/DOUM.png';
 import club5 from '../../assets/logos/KOSS.png';
 import club6 from '../../assets/logos/KPSC.png';
 import club7 from '../../assets/logos/WINK.png';
@@ -12,7 +12,7 @@ const logoList = [club1, club2, club3, club4, club5, club6, club7];
 
 function ClubCarousel() {
   const extendedLogos = [...logoList, ...logoList];
-
+  // const extendedLogos = [...logoList];
   return (
     <div className="carousel-container">
       <div className="carousel-track">
